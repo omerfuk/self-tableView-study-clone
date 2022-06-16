abf8929263f7ac95f9ea596ef36935c09a867a92
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-16 22:30:59 +0300

    Initial Commit

---
+++ Programmatic Study Self/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic Study Self
+//
+//  Created by Ömer Faruk Kılıçaslan on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
